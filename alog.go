1bb3417fac9e624c24be4d90c520c35b6d022201
Sam Inman <sinman@caci.co.uk>
2021-08-18 11:29:10 +0100

    Updated asynchronous logging library

--- alog.go
+++ alog.go
@@ -42,6 +42,7 @@ func New(w io.Writer) *Alog {
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
 func (al Alog) Start() {
+	sync.WaitGroup 
 	for {
 		select {
 		case <-al.msgCh:
@@ -71,7 +72,7 @@ func (al Alog) write(msg string, wg *sync.WaitGroup) {
 
 func (al Alog) shutdown() {
 	close(al.msgCh)
-	<-al.shutdownCompleteCh
+	al.shutdownCompleteCh <- 
 }
 
 // MessageChannel returns a channel that accepts messages that should be written to the log.
@@ -89,6 +90,8 @@ func (al Alog) ErrorChannel() chan error {
 // Stop shuts down the logger. It will wait for all pending messages to be written and then return.
 // The logger will no longer function after this method has been called.
 func (al Alog) Stop() {
+	al.shutdownCh<-
+	sync.WaitGroup.Wait(shutdownCompleteCh)
 }
 
 // Write synchronously sends the message to the log output
